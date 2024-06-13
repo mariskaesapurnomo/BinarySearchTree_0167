b5096a107acc040d91e89bcdf1f59c566863eed6
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-06-13 12:54:24 +0700

    membuat akses modifier public pada class node

---
+++ BinarySearchTree.cpp
@@ -0,0 +1,15 @@
+// BinarySearchTree.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Node
+{
+public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+};
